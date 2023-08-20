e929d2cf70d564310361bfc75c9a30c019cf96ba
Gustavo Alexadre Silva <alexandregustavo00@gmail.com>
2023-08-20 17:43:20 -0300

    Questao 1 identificar se é primo ou nao

--- lista/questao1.c
+++ lista/questao1.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
+int primo(int n, int divisor) {
+   
+    if (n < 2) return 0;
+        
+    if (divisor == 1) return 1;
+        
+    if (n % divisor == 0) return 0;
+        
+    return primo(n, divisor - 1);
+}
+
 int main(){
-    printf("Teste\n");
+
+    int num;
+
+    scanf("%d",&num);
+
+    int result = primo(num, num-1);
+
+    if(result == 1) printf("E primo\n");
+
+    else printf("Nao e primo\n");
 
     return 0;
-}
+}   
