3e6b5d4f8079fd6c3f154052618d564f2984cceb
karllisson <karllisson@doity.com.br>
2023-08-20 20:41:38 -0300

    feat: adiciona decomposição em numeros primos

---
+++ lista/questao3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>;
+
+void decompor_em_numeros_primos(int numero) 
+{
+    int contador;
+    while (numero % 2 == 0) {
+        printf("2 ");
+        numero /= 2;
+    }
+    
+    for (contador = 3; (contador * contador) <= numero; contador += 2) {
+        while (numero % contador == 0) {
+            printf("%d ", contador);
+            numero /= contador;
+        }
+    }
+    
+    if (numero > 2) {
+        printf("%d ", numero);
+    }
+}
+
+int main() 
+{
+    int numero;
+    printf("Entre com um numero inteiro: ");
+    scanf("%d", &numero);
+    
+    printf("Fatores primos de %d: ", numero);
+    decompor_em_numeros_primos(numero);
+    
+    return 0;
+}
