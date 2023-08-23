c6e8ff54623fe04126ac1b76e703584b50109ae0
Gustavo Alexadre Silva <alexandregustavo00@gmail.com>
2023-08-23 08:54:30 -0300

    adiciona listar numeros primos sequencialmente

--- lista/questao2.c
+++ lista/questao2.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#include <time.h>
+#include <math.h>
 
-int main(){
+int findprimo(int n, int divisor) {
+
+    if(divisor == 1) return 1;
+
+    if (n % divisor == 0) return 0;
+    
+    return findprimo(n, divisor - 1); 
+}
+int checkprimo(int n){
+    
+    if(n < 2) return 0;
+
+    int maiordiv = sqrt(n);
+    
+    return findprimo(n,maiordiv);
+}
+
+int main() {
+    
+    time_t tempoInicio = time(NULL);
+    int numero = 2; 
+
+    while (1) {
+       
+        if (checkprimo(numero)) printf("%d\n", numero);
+            
+        if (time(NULL) - tempoInicio == 60) break;
+
+        numero++;
+    }
 
     return 0;
-    
 }
