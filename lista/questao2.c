85e16a60e4c010e88dd54aea6c6543791b376c63
Gustavo Alexadre Silva <alexandregustavo00@gmail.com>
2023-08-23 16:00:02 -0300

    modificação da questao2

--- lista/questao2.c
+++ lista/questao2.c
@@ -3,35 +3,28 @@
 #include <math.h>
 
 int findprimo(int n, int divisor) {
-
-    if(divisor == 1) return 1;
-
+    if (divisor == 1) return 1;
     if (n % divisor == 0) return 0;
-    
     return findprimo(n, divisor - 1); 
 }
-int checkprimo(int n){
-    
-    if(n < 2) return 0;
 
+int checkprimo(int n) {
+    if (n < 2) return 0;
     int maiordiv = sqrt(n);
-    
-    return findprimo(n,maiordiv);
+    return findprimo(n, maiordiv);
 }
 
 int main() {
-    
     time_t tempoInicio = time(NULL);
-    int numero = 2; 
+    int numero = 2;
 
     while (1) {
-       
         if (checkprimo(numero)) printf("%d\n", numero);
-            
-        if (time(NULL) - tempoInicio == 60) break;
+
+        if (time(NULL) - tempoInicio >= 60) break;
 
         numero++;
     }
-
+    
     return 0;
 }
