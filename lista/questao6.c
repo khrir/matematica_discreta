d42775fcc1a6c2a62d5886ae67fe77546edd618f
karllisson <karllisson@doity.com.br>
2023-08-29 19:02:12 -0300

    feat: adiciona o cálculo da relação de Bézout

---
+++ lista/questao6.c
@@ -0,0 +1,29 @@
+#include <stdio.h>;
+
+int calcular_mdc_estendido(int a, int b, int *coefS, int *coefT)
+{
+    if(b == 0) {
+        *coefS = 1;
+        *coefT = 0;
+        return a;
+    }
+
+    int s1, t1;
+    int mdc = calcular_mdc_estendido(b, a % b, &s1, &t1);
+
+    *coefS = t1;
+    *coefT = s1 - (a / b) * t1;
+
+    return mdc;
+}
+
+int main()
+{
+    int valorA, valorB, coefS, coefT;
+
+    printf("Entre com dois valores: ");
+    scanf("%d %d", &valorA, &valorB);
+
+    int mdc = calcular_mdc_estendido(valorA, valorB, &coefS, &coefT);
+    printf("MDC: %d\ncoeficiente s: %d\ncoeficiente t: %d\n", mdc, coefS, coefT);
+}
